b56f611592cadfb7271aeef05374485e96fd6033
gokuseii <92649326+gokuseii@users.noreply.github.com>
2023-02-20 16:14:04 +0200

    Add check for even data length

--- internal/service/requests/call_contract.go
+++ internal/service/requests/call_contract.go
@@ -2,15 +2,21 @@ package requests
 
 import (
 	"encoding/json"
-	"net/http"
-	"regexp"
-
 	"github.com/Swapica/relayer-svc/resources"
 	val "github.com/go-ozzo/ozzo-validation/v4"
 	"gitlab.com/distributed_lab/logan/v3/errors"
+	"net/http"
+	"regexp"
 )
 
 var hexRegexp = regexp.MustCompile("^0x[0-9a-f]+$")
+var hexLengthRule = func(value interface{}) error {
+	data := value.(string)
+	if len(data)%2 != 0 {
+		return errors.New("data length must be even")
+	}
+	return nil
+}
 
 func NewCallContractRequest(r *http.Request) (resources.EvmTransactionRequest, error) {
 	var dst resources.EvmTransactionRequest
@@ -20,7 +26,7 @@ func NewCallContractRequest(r *http.Request) (resources.EvmTransactionRequest, e
 
 	return dst, val.Errors{
 		"data/type":             val.Validate(dst.Data.Type, val.Required, val.In(resources.EVM_TRANSACTION)),
-		"data/attributes/data":  val.Validate(dst.Data.Attributes.Data, val.Required, val.Match(hexRegexp)),
+		"data/attributes/data":  val.Validate(dst.Data.Attributes.Data, val.Required, val.Match(hexRegexp), val.By(hexLengthRule)),
 		"data/attributes/chain": val.Validate(dst.Data.Attributes.Chain, val.Required),
 	}.Filter()
 }
