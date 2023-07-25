735a64d8719c3db3a99a996a48f399e01e2402a6
roman_harazha <roman.harazha@gmail.com>
2023-07-25 11:42:12 +0300

    check commission percent before signing and transacting

--- internal/service/handlers/call_contract.go
+++ internal/service/handlers/call_contract.go
@@ -1,13 +1,14 @@
 package handlers
 
 import (
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"gitlab.com/distributed_lab/logan/v3"
+	"math/big"
 	"net/http"
 
 	"github.com/Swapica/relayer-svc/internal/service/requests"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
+	"gitlab.com/distributed_lab/logan/v3"
 )
 
 func CallContract(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +26,7 @@ func CallContract(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// check if token chain exists
 	tChain := TokenChains(r).Get("0x"+request.Data.Attributes.Data[90:130], request.Data.Attributes.Chain)
 	if tChain == nil {
 		Log(r).WithFields(logan.F{
@@ -32,6 +34,19 @@ func CallContract(w http.ResponseWriter, r *http.Request) {
 			"chain": request.Data.Attributes.Chain,
 		}).Debug("non-existent token chain")
 		ape.RenderErr(w, problems.NotFound())
+		return
+	}
+
+	// check if commission is not >= 100%
+	commission, _ := new(big.Int).SetString(request.Data.Attributes.Data[130:194], 16)
+
+	base := big.NewInt(10)
+	exponent := big.NewInt(27)
+	result := new(big.Int).Exp(base, exponent, nil)
+
+	if commission.Cmp(result) > -1 {
+		Log(r).WithField("commission", commission.String()).Error("commission is too high")
+		ape.RenderErr(w, problems.InternalError())
 		return
 	}
 
